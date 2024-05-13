a761696674e76aecc885c3cf4af72c0e5a60a1b3
EddieKrr <eddieserena.karira@strathmore.edu>
2024-05-13 17:27:49 +0300

    Made the JS inline so it works

---
+++ FirstWebsite/Home.js
@@ -0,0 +1,14 @@
+//Width of side nav
+function openNav()
+{
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+// Closing width of side nav
+function closeNav()
+{
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
